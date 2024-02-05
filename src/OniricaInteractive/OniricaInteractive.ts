54a40e346bdc025276758798b1d0bdafd5447872
Matteo Amerena <matteo.amerena@fusefactory.it>
2024-02-05 16:10:46 +0100

    added light font, added kd-tree sorting

--- src/OniricaInteractive/OniricaInteractive.ts
+++ src/OniricaInteractive/OniricaInteractive.ts
@@ -5,23 +5,44 @@ import { Resource } from '../engine/Resources'
 import { Dream } from './Dream';
 import Papa from 'papaparse';
 import { TextUI } from './UI/Text';
+import { Text } from 'troika-three-text'
+import { kdTree } from 'kd-tree-javascript'
 
 export class OniricaInteractive implements Experience {
+    private nneighbors: number = 10
     private textUI: TextUI = new TextUI();
     private dreams: Dream[] = [];
     private mesh: THREE.InstancedMesh | undefined;
+    private cameraDir: THREE.Vector3 = new THREE.Vector3();
+    private selectedDreams: Text[] = [];
+
     private selectedId: number = -1
     private highlightedIds: number[] = []
     private highlightSphere: THREE.Mesh | undefined
+    private tree: kdTree<THREE.Vector3> | undefined
 
     public baseColor = new THREE.Color();
     public highlightColor = new THREE.Color(0xf57614)
     resources: Resource[] = [
     ]
 
+
     constructor(private engine: Engine) {
         this.parseCSV().finally(() => {
             this.createScene()
+            //now the mesh has been created, initialize kd tree
+
+            var distance = function (a: THREE.Vector3, b: THREE.Vector3) {
+                var deltaX = b.x - a.x;
+                var deltaY = b.y - a.y;
+                var deltaZ = b.z - a.z;
+
+                var distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+
+                return distance;
+            }
+
+            this.tree = new kdTree(this.dreams.map(a => a.position), distance, ["x", "y", "z"]);
         })
     }
 
@@ -43,11 +64,9 @@ export class OniricaInteractive implements Experience {
         const field = document.getElementById('userInput') as HTMLInputElement;
         const search = document.getElementById('button-search') as HTMLButtonElement;
 
-        field.addEventListener("keypress", function(event:KeyboardEvent) {
+        field.addEventListener("keypress", function (event: KeyboardEvent) {
             if (event.key === "Enter") {
-                // Cancel the default action, if needed
                 event.preventDefault();
-                // Trigger the button element with a click
                 search.click();
             }
         })
@@ -55,7 +74,6 @@ export class OniricaInteractive implements Experience {
         search.addEventListener('click', () => {
             const inputValue = field.value;
             this.highlightedIds = this.search(inputValue, this.dreams)
-            console.log(inputValue)
             if (this.highlightedIds) {
                 for (let i = 0; i < this.dreams.length; i++) {
                     if (this.highlightedIds.includes(i))
@@ -66,11 +84,11 @@ export class OniricaInteractive implements Experience {
 
             }
 
-            this.projectUmap(inputValue).then((result: any) => {
-                this.highlightSphere!.position.x = parseFloat(result.x);
-                this.highlightSphere!.position.y = parseFloat(result.y);
-                this.highlightSphere!.position.z = parseFloat(result.z);
-            })
+            // this.projectUmap(inputValue).then((result: any) => {
+            //     this.highlightSphere!.position.x = parseFloat(result.x);
+            //     this.highlightSphere!.position.y = parseFloat(result.y);
+            //     this.highlightSphere!.position.z = parseFloat(result.z);
+            // })
         })
     }
 
@@ -93,7 +111,34 @@ export class OniricaInteractive implements Experience {
 
     // called on each render
     update() {
+        this.cameraDir = this.engine.camera.instance.getWorldDirection(this.cameraDir).normalize()
+        let stepDistance = 0.5
+        let futurePos = this.engine.camera.instance.position.clone().add(this.cameraDir.clone().multiplyScalar(stepDistance));
 
+        let tmp = this.tree?.nearest(futurePos, this.nneighbors).map((p: any) => {
+            return this.dreams.map(d => d.position).indexOf(p[0])
+        })
+        if (tmp) {
+            this.highlightedIds = tmp
+            for (let i = 0; i < this.dreams.length; i++) {
+                if (this.highlightedIds.includes(i))
+                    {
+                        this.mesh!.setColorAt(i, this.highlightColor);
+                    }
+                else {
+                        this.mesh!.setColorAt(i, this.baseColor);
+                }
+            }
+            this.mesh!.instanceColor!.needsUpdate = true
+            for (let i = 0; i < this.nneighbors; i++) {
+                const currentDream:Dream = this.dreams[this.highlightedIds[i]]
+                this.selectedDreams.at(i).text = currentDream.dreamReport.substring(0, 50)
+                this.selectedDreams.at(i).position.x = currentDream.position.x
+                this.selectedDreams.at(i).position.y = currentDream.position.y + 0.02
+                this.selectedDreams.at(i).position.z = currentDream.position.z
+                this.selectedDreams.at(i).lookAt(this.engine.camera.instance.position)
+            }
+        }
     }
 
     async parseCSV() {
@@ -123,7 +168,21 @@ export class OniricaInteractive implements Experience {
     }
 
     createScene() {
-        const geometry = new THREE.IcosahedronGeometry(0.015, 1);
+        const geometry = new THREE.IcosahedronGeometry(0.008, 1);
+        this.selectedDreams = new Array(5).fill(null);          
+          
+        //create texts
+        for (let i = 0; i < this.nneighbors; i++) {
+            const myText:Text = new Text();
+            this.engine.scene.add(myText);
+            myText.text = ""
+            myText.font = "./assets/fonts/MartianMono-Regular.ttf"
+            myText.fontSize = 0.01;
+            myText.color = this.highlightColor;
+            myText.sync();
+            this.selectedDreams[i] = myText;
+        }
+
         let material = new THREE.MeshStandardMaterial({
             color: 0xbebeb6,
             transparent: true,
@@ -150,7 +209,7 @@ export class OniricaInteractive implements Experience {
         const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
         directionalLight2.position.set(-5, 5, -5);
         this.engine.scene.add(directionalLight, directionalLight2);
-        
+
         //sphere
         const sphereMat = new THREE.MeshBasicMaterial({
             color: this.highlightColor,
@@ -160,14 +219,14 @@ export class OniricaInteractive implements Experience {
         const geo = new THREE.SphereGeometry(0.25, 32, 32);
         this.highlightSphere = new THREE.Mesh(geo, sphereMat);
         this.highlightSphere.translateX(10000);
-        this.engine.scene.add(this.highlightSphere)    
+        this.engine.scene.add(this.highlightSphere)
 
     }
 
     search(word: string, dreams: Dream[]): number[] {
         const indices: number[] = [];
         dreams.forEach((dream, index) => {
-            if (dream.dreamReport.includes(word)) {
+            if (dream.dreamReport.includes(" " + word + " ")) {
                 indices.push(index);
             }
         });
